ae8952a98752514d29f782448b51dc9e73ddd4c2
folkertdebonnet <99896540+folkertdebonnet@users.noreply.github.com>
2024-01-04 15:15:24 +0100

    map hosting

--- map.js
+++ map.js
@@ -44,7 +44,7 @@ async function generateMap() {
       async function get() {
         console.log("fetch");
         try {
-          const response = await fetch("http://localhost:3000/data");
+          const response = await fetch("https://back-end-hosting.onrender.com/data");
           const fetchedData = await response.json();
           data = fetchedData;
           console.log(data);
